94c99442c2b004fe92a9d0263c00bd78f1b13686
고윤석 <rhdbstjr123@nate.com>
2022-12-07 15:00:44 +0900

    token

--- src/Study/Test02.java
+++ src/Study/Test02.java
@@ -3,5 +3,6 @@ package Study;
 public class Test02 {
 	public static void main(String[] args) {
 		System.out.println("Git Pust Test");
+		System.out.println("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");
 	}
 }
